3f1719fdbf930088a22982feb147a0a62e6c2db2
palage4a <vvnvvplg@gmail.com>
2022-11-14 22:47:33 +0300

    chapter 7: evaluation

--- evaluation.c
+++ evaluation.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mpc/mpc.h"
 
@@ -15,6 +16,32 @@ char* readline(char *prompt) {
 #else
 #include <editline/readline.h>
 #endif
+
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) return x + y;
+  if (strcmp(op, "-") == 0) return x - y;
+  if (strcmp(op, "*") == 0) return x * y;
+  if (strcmp(op, "/") == 0) return x / y;
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
 
 int main(int argc, char **argv) {
   mpc_parser_t *Number = mpc_new("number");
@@ -40,6 +67,8 @@ lispy: /^/ <operator> <expr>+ /$/ ;            \
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r )) {
       mpc_ast_print(r.output);
+      long res = eval(r.output);
+      printf("%li\n", res);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
